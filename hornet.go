5842f3fe16e86f11e8674421a7be3375255c084d
Jared Kofron <jared.kofron@gmail.com>
2015-03-31 16:23:47 -0700

    change to pass golint

--- hornet.go
+++ hornet.go
@@ -1,32 +1,32 @@
 /*
- * hornet - a tool for nearline processing of Project 8 triggered data 
- * 
- *  https://github.com/kofron/hornet
- * 
- * hornet uses inotify to start the processing of triggered data from the
- * tektronix RSA.  it tries to stay out of the way by doing one job and
- * doing it well, so that it is compatible with other workload management
- * tools.  
- * 
- * all you need to tell it is the size of its thread pool for incoming files,
- * the configuration file you want to run, and the path to Katydid.  it will
- * handle the rest.
- *
- * for the most up to date usage information, do
- *
- *   hornet -h
- *
- */
+* hornet - a tool for nearline processing of Project 8 triggered data
+*
+*  https://github.com/kofron/hornet
+*
+* hornet uses inotify to start the processing of triggered data from the
+* tektronix RSA.  it tries to stay out of the way by doing one job and
+* doing it well, so that it is compatible with other workload management
+* tools.
+*
+* all you need to tell it is the size of its thread pool for incoming files,
+* the configuration file you want to run, and the path to Katydid.  it will
+* handle the rest.
+*
+* for the most up to date usage information, do
+*
+*   hornet -h
+*
+*/
 package main
 
 import (
-	"flag"
-	"log"
-	"fmt"
-	"os"
-	"io/ioutil"
 	"bytes"
 	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
 )
 
 // Global config
@@ -34,43 +34,43 @@ var (
 	MaxPoolSize uint = 25
 )
 
-// Configuration data.  Changeable from the command line.
+// Config represents the user-specified configuration data of hornet
 type Config struct {
-	PoolSize uint
-	KatydidPath string
+	PoolSize        uint
+	KatydidPath     string
 	KatydidConfPath string
 }
 
-// validate the config of hornet.  this means that:
+// Validate checks the sanity of a Config instance
 //   1) the number of threads is sane
 //   2) the provided path to katydid actually points at an executable
 //   3) the provided config file is parsable json
 func (c Config) Validate() (e error) {
 	if c.PoolSize > MaxPoolSize {
-		e = fmt.Errorf("Size of thread pool cannot exceed %d!",MaxPoolSize)
+		e = fmt.Errorf("Size of thread pool cannot exceed %d!", MaxPoolSize)
 	}
 
-	if exec_info, exec_err := os.Stat(c.KatydidPath); exec_err == nil {
-		if (exec_info.Mode() & 0111) == 0 {
+	if execInfo, execErr := os.Stat(c.KatydidPath); execErr == nil {
+		if (execInfo.Mode() & 0111) == 0 {
 			e = fmt.Errorf("Katydid does not appear to be executable!")
 		}
 	} else {
 		e = fmt.Errorf("error when examining Katydid executable: %v",
-			exec_err)
+			execErr)
 	}
 
-	if conf_bytes, conf_err := ioutil.ReadFile(c.KatydidConfPath); conf_err == nil {
+	if confBytes, confErr := ioutil.ReadFile(c.KatydidConfPath); confErr == nil {
 		v := make(map[string]interface{})
-		conf_decoder := json.NewDecoder(bytes.NewReader(conf_bytes))
-		if decode_err := conf_decoder.Decode(&v); decode_err != nil {
+		confDecoder := json.NewDecoder(bytes.NewReader(confBytes))
+		if decodeErr := confDecoder.Decode(&v); decodeErr != nil {
 			e = fmt.Errorf("Katydid config appears unparseable... %v",
-				decode_err)
+				decodeErr)
 		}
 	} else {
 		e = fmt.Errorf("error when opening Katydid config: %v",
-			conf_err)
+			confErr)
 	}
-	
+
 	return
 }
 
@@ -80,7 +80,7 @@ func main() {
 	conf := Config{}
 
 	// set up flag to point at conf, parse arguments and then verify
-	flag.UintVar(&conf.PoolSize, 
+	flag.UintVar(&conf.PoolSize,
 		"pool-size",
 		20,
 		"size of worker pool")
@@ -94,9 +94,17 @@ func main() {
 		"full path to Katydid config file to use when processing")
 	flag.Parse()
 
-	if config_err := conf.Validate(); config_err != nil {
+	if configErr := conf.Validate(); configErr != nil {
 		flag.Usage()
-		log.Fatal("(FATAL) ",config_err)
+		log.Fatal("(FATAL) ", configErr)
 	}
-	
+
+	// if we've made it this far, it's time to get down to business.
+	// we'll start an inotify Notify request for the working directory.
+	// then we have a single channel that we use to communicate with the
+	// worker pool.  as we get file closed events, we check to see if we
+	// already know about the file in question.  if not, and it is a
+	// data file, we'll enqueue it to get chewed on by the next available
+	// worker thread.
+
 }
