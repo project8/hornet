09e4dbf5c5f4fa5d0d5263d7326de93f3c6ec3f6
Jared Kofron <jared.kofron@gmail.com>
2015-03-31 17:10:29 -0700

    rudimentary version working, it does nothing but log names

--- worker.go
+++ worker.go
@@ -2,3 +2,19 @@
 * worker.go
 */
 package main
+
+import (
+	"log"
+)
+
+// Worker waits for strings on a channel, and launches a Katydid process for
+// each string it receives, which should be the name of the file to process.
+func Worker(context Context, config Config) {
+	// Put off being done until there's nothing left in the channel
+	// to process
+	defer context.Pool.Done()
+
+	for f := range context.FilePipeline {
+		log.Print(f)
+	}
+}
