3390c44982b61e838dfec644ec0ef27494584e78
Jared Kofron <jared.kofron@gmail.com>
2015-04-09 09:46:35 -0400

    add skeleton of mover thread.

---
+++ mover.go
@@ -0,0 +1,35 @@
+/*
+* mover.go
+* 
+* the mover thread moves files from a source location to a destination.
+* it can batch files for moving if requested.
+*/
+package main
+
+import (
+	"log"
+)
+
+// Mover receives filenames over an unbuffered channel, and moves them from
+// their current place on the filesystem to a destination.  If so configured
+// it will wait (up to a timeout) until it has some number of files to move
+// in a batch.  It is stopped when it receives a message from the main thread
+// to shut down.
+func Mover(context Context, config Config) {
+	defer context.Pool.Done()
+	
+moveLoop:
+	for {
+		select {
+			// the control messages can stop execution
+			// TODO: should finish pending jobs before dying.
+		case controlMsg := <- context.Control:
+			if controlMsg == StopExecution {
+				log.Print("mover stopping on interrupt.")
+				break moveLoop
+			}
+		}
+	}
+
+	log.Print("mover finished.")
+}
