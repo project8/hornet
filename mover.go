b2bc56dc70a57bb88d4fe86d5ddc699448c2bd30
Jared Kofron <jared.kofron@gmail.com>
2015-04-20 14:16:46 -0700

    now makes subdirectories as necessary to move files

--- mover.go
+++ mover.go
@@ -7,10 +7,14 @@
 package main
 
 import (
+	"errors"
 	"io"
 	"log"
 	"os"
+	"path/filepath"
 )
+
+type DirectorySet map[string]bool
 
 // copy will copy the contents of one file to another.  the arguments are both
 // strings i.e. paths to the original and the desired destination.  if something
@@ -35,13 +39,36 @@ func copy(source, destination string) error {
 	return nil
 }
 
+// Move moves a file from one place to another.  This is equivalent to
+// copy-and-delete.
+func Move(src, dest string) (e error) {
+	if copyErr := copy(src, dest); copyErr != nil {
+		log.Printf("file copy failed! (%v -> %v) [%v]\n",
+			src, dest, copyErr)
+		e = errors.New("failed to copy file")
+	} else {
+		if rmErr := os.Remove(src); rmErr != nil {
+			log.Printf("file rm failed! (%v) [%v]\n",
+				src,
+				rmErr)
+			e = errors.New("failed to remove old file")
+		}
+	}
+
+	return
+}
+
 // Mover receives filenames over an unbuffered channel, and moves them from
 // their current place on the filesystem to a destination.  If so configured
 // it will wait (up to a timeout) until it has some number of files to move
 // in a batch.  It is stopped when it receives a message from the main thread
 // to shut down.
 func Mover(context Context, config Config) {
+	// decrement the wg counter at the end
 	defer context.Pool.Done()
+
+	// keep a running list of all of the directories we know about.
+	ds := make(DirectorySet)
 
 moveLoop:
 	for {
@@ -54,17 +81,28 @@ moveLoop:
 				break moveLoop
 			}
 		case newFile := <-context.FinishedFileStream:
-			destName := MovedFilePath(newFile, config.DestDirPath)
-			if copyErr := copy(newFile, destName); copyErr != nil {
-				log.Printf("file copy failed! (%v -> %v) [%v]\n",
-					newFile, destName, copyErr)
+			destName, destErr := RenamePathRelativeTo(newFile,
+				config.WatchDirPath,
+				config.DestDirPath)
+			if destErr != nil {
+				log.Printf("bad rename request: %s -> %s w.r.t %s\n",
+					newFile, config.DestDirPath, config.WatchDirPath)
 			} else {
-				if rmErr := os.Remove(newFile); rmErr != nil {
-					log.Printf("file rm failed! (%v) [%v]\n",
-						newFile,
-						rmErr)
+				// check if we already know about the destdir
+				newDir := filepath.Dir(destName)
+				if ds[newDir] == false {
+					if mkErr := os.MkdirAll(newDir, os.ModeDir); mkErr != nil {
+						log.Printf("couldn't make directory %v: [%v]",
+							newDir, mkErr)
+					}
+					copy(newFile, destName)
+				}
+				if moveErr := Move(newFile, destName); moveErr != nil {
+					log.Printf("error moving (%v -> %v) [%v]",
+						newFile, destName, moveErr)
 				}
 			}
+
 		}
 
 	}
